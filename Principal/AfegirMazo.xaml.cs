ee3e509591126f40eb4130732ec98718bfdac758
Jordi <jorditrabajos1@hotmail.com>
2023-05-17 12:18:35 +0200

    Metodes afegir mazo i disseny nou de la finestra extend

--- Principal/AfegirMazo.xaml.cs
+++ Principal/AfegirMazo.xaml.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -22,6 +23,9 @@ namespace Principal
     {
         //Atributs
         private Usuari usuari;
+        private Cartes cartesMazoNou;
+        private Cartes totesCartes;
+        private int contador;
         //Constructors
         /// <summary>
         /// Constructor d'afegir mazo que rep un usuari.
@@ -30,6 +34,9 @@ namespace Principal
         public AfegirMazo(Usuari usuari)
         {
             InitializeComponent();
+            Usuari = usuari;
+            CartesMazoNou = new();
+            contador = 1;
         }
         //Propietats
         /// <summary>
@@ -40,6 +47,11 @@ namespace Principal
             get { return this.usuari; }
             set { this.usuari = value; }
         }
+        public Cartes CartesMazoNou
+        {
+            get { return this.cartesMazoNou; }
+            set { this.cartesMazoNou = value; }
+        }
         /// <summary>
         /// Métode que carrega totes les cartes al stackpanel.
         /// </summary>
@@ -49,12 +61,14 @@ namespace Principal
         private void stckPanelCartes_Loaded(object sender, RoutedEventArgs e)
         {
             //Accedeixo a Cartes per recuperar les cartes de la BD.
-            Cartes cartes = new();
-            cartes.LlistaCartes = cartes.RecuperarCartes();
-            foreach (Carta carta in cartes.LlistaCartes)
+            totesCartes = new();
+            totesCartes.LlistaCartes = totesCartes.RecuperarCartes();
+            foreach (Carta carta in totesCartes.LlistaCartes)
             {
                 ListBox cartaList = new();
+                cartaList.HorizontalContentAlignment = HorizontalAlignment.Center;
                 cartaList.Width = 250;
+                cartaList.Height = 450;
                 //Afegeixo el nom de la carta.
                 Label nomCarta = new();
                 nomCarta.Content = carta.Nom;
@@ -68,6 +82,8 @@ namespace Principal
                 descripcioCarta.Content = carta.Descripcio;
                 //Afegeixo un listbox amb les habilitats.
                 ListBox habilitatsCarta = new();
+                habilitatsCarta.HorizontalContentAlignment = HorizontalAlignment.Center;
+                habilitatsCarta.Width = 200;
                 habilitatsCarta.Items.Add(carta.Habilitats.LListahabilitats[0].Nom);
                 habilitatsCarta.Items.Add(carta.Habilitats.LListahabilitats[1].Nom);
                 habilitatsCarta.Items.Add(carta.Habilitats.LListahabilitats[2].Nom);
@@ -75,25 +91,52 @@ namespace Principal
                 //Botó que fará que s'afegeixi la carta.
                 Button botoMazo = new();
                 botoMazo.Content = "Afegir " + carta.Nom;
-                //Guardo l'id de la carta per afegirla.
-                Label idCarta = new();
-                idCarta.Content = carta.Id;
-                idCarta.Visibility = Visibility.Hidden;
                 //Afegir els elements anteriors al listbox.
                 cartaList.Items.Add(nomCarta);
                 cartaList.Items.Add(imatgeCarta);
                 cartaList.Items.Add(descripcioCarta);
                 cartaList.Items.Add(habilitatsCarta);
                 cartaList.Items.Add(botoMazo);
-                cartaList.Items.Add(idCarta);
-
                 //Afegeixo el listbox amb totes les dades al listbox contenidor.
                 stckPanelCartes.Children.Add(cartaList);
+                //Afegeixo un event al boto
+                botoMazo.Click += (sendr, EventArgs) => { AfegirCarta(sendr, EventArgs, carta.Id); };
+
             }
+
 
         }
 
         //Métodes
+        private void AfegirCarta(object sender, RoutedEventArgs e, int id)
+        {
+            try
+            {
+                if (contador != 5)
+                {
+                    CartesMazoNou.LlistaCartes.Add(totesCartes.LlistaCartes[id - 1]);
+                    contador++;
+                    lblCartesAfegides.Content += " " + totesCartes.LlistaCartes[id - 1].Nom + ",";
+                }
+                else if(contador == 5)
+                {
+                    lblCartesAfegides.Content = lblCartesAfegides.Content.ToString().Substring(0, lblCartesAfegides.Content.ToString().Length - 1);
+                    lblCartesAfegidesSucces.Visibility = Visibility.Visible;
+                    Mazos mazos = new();
+                    mazos.LlistaMazos = mazos.RecuperarMazos();
+                    AfegirMazoExtend extend = new(CartesMazoNou, Usuari, mazos.LlistaMazos.Count);
+                    extend.Show();
+                    this.Close();
+                    
+                }
 
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("No s'ha pogut afegir la carta selccionada.");
+            }
+
+
+        }
     }
 }
---
+++ Principal/AfegirMazoExtend.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Principal.Negoci;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Principal
+{
+    /// <summary>
+    /// Lógica de interacción para AfegirMazoExtend.xaml
+    /// </summary>
+    public partial class AfegirMazoExtend : Window
+    {
+        private Cartes cartes;
+        private Usuari usuari;
+        private int id;
+        public AfegirMazoExtend(Cartes cartes,Usuari usuari, int id)
+        {
+            InitializeComponent();
+            this.cartes = cartes;
+            this.usuari = usuari;
+            this.id = id;
+        }
+
+        private void btnAfegirNouMazoExtend_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Mazo mazo = new(this.cartes, txtBoxNomMazo.Text, this.usuari, this.id);
+                this.usuari.Mazos.LlistaMazos.Add(mazo);
+                MessageBox.Show("Mazo afegit correctament.");
+                Home home = new(this.usuari);
+                home.Show();
+                this.Close();
+            }catch(Exception ex)
+            {
+                MessageBox.Show("No s'ha pogut afegir el mazo.");
+            }
+            
+        }
+    }
+}
